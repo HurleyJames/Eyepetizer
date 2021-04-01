10558af04e453dc364cf3b57677a2b590e37b2db
Hurley <1401682479@qq.com>
2021-04-01 15:51:59 +0800

    feat: update toolbar displaying in the setting activity

--- module_user/src/main/java/ac/hurley/module_user/activity/SettingActivity.kt
+++ module_user/src/main/java/ac/hurley/module_user/activity/SettingActivity.kt
@@ -1,6 +1,7 @@
 package ac.hurley.module_user.activity
 
 import ac.hurley.module_common.util.immersionStatusBar
+import ac.hurley.module_common.util.setToolBar
 import ac.hurley.module_provider.router.RouterPath
 import ac.hurley.module_user.databinding.UserSettingActivityBinding
 import ac.hurley.module_user.viewmodel.SettingViewModel
@@ -9,6 +10,7 @@ import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.alibaba.android.arouter.facade.annotation.Route
+import kotlinx.android.synthetic.main.user_setting_activity.*
 
 /**
  * <pre>
@@ -26,13 +28,13 @@ class SettingActivity : AppCompatActivity() {
     private val mViewModel by lazy { ViewModelProvider(this).get(SettingViewModel::class.java) }
 
 
-
 //    private lateinit var mSettingViewModel: SettingViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
         immersionStatusBar(true, android.R.color.white, true, 0.2f)
+        setToolBar(tl_setting)
         mBinding.viewModel = mViewModel
         mBinding.lifecycleOwner = this
     }
